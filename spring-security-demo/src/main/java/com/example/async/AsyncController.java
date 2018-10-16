48851d14c01037864f787122d4e2eb512897eeb4
baby7305 <songhaibei7305@163.com>
2018-10-16 15:28:19 +0800

    Callable异步处理，Runnable进化版

--- spring-security-demo/src/main/java/com/example/async/AsyncController.java
+++ spring-security-demo/src/main/java/com/example/async/AsyncController.java
@@ -6,21 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @RestController
 @RequestMapping("/order")
-public class AsyncController {
+public class AsyncController implements Callable<String> {
     private Logger logger = LoggerFactory.getLogger(AsyncController.class);
 
     @GetMapping
     public String order() {
         logger.info("主线程开始");
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
+            this.call();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         logger.info("主线程结束");
         return "success";
     }
 
+    @Override
+    public String call() throws Exception {
+        logger.info("副线程开始");
+        Thread.sleep(1000);
+        logger.info("副线程返回");
+        return "success";
+    }
 }
