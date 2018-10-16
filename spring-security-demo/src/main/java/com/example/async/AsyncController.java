0be60d36b3797bc7c97ea983ef8a6826696f9079
baby7305 <songhaibei7305@163.com>
2018-10-16 18:07:34 +0800

    deferredResultHolder，模拟订单提交异步处理

--- spring-security-demo/src/main/java/com/example/async/AsyncController.java
+++ spring-security-demo/src/main/java/com/example/async/AsyncController.java
@@ -1,14 +1,24 @@
 package com.example.async;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 @RequestMapping("/order")
 public class AsyncController {
+
+    @Autowired
+    private MockQueue mockQueue;
+
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
+
     private Logger logger = LoggerFactory.getLogger(AsyncController.class);
 
     @GetMapping
@@ -21,6 +31,22 @@ public class AsyncController {
         }
         logger.info("主线程结束");
         return "success";
+    }
+
+    @GetMapping("/deferredResultHolder")
+    public DeferredResult<String> order1() {
+
+        logger.info("主线程开始");
+
+        String orderNumber = RandomStringUtils.randomNumeric(8);
+        mockQueue.setPlaceOrder(orderNumber);
+
+        DeferredResult<String> result = new DeferredResult<>();
+        deferredResultHolder.getMap().put(orderNumber, result);
+
+        logger.info("主线程结束");
+        
+        return result;
     }
 
 }
