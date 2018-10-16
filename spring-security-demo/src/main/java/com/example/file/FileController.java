592db757b9f9e7de2ccede1785d9cbe9b7085b7c
baby7305 <songhaibei7305@163.com>
2018-10-16 12:19:07 +0800

    文件下载

--- spring-security-demo/src/main/java/com/example/file/FileController.java
+++ spring-security-demo/src/main/java/com/example/file/FileController.java
@@ -1,12 +1,16 @@
 
 package com.example.file;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 
 @RestController
@@ -26,6 +30,21 @@ public class FileController {
 
 		String path = localFile.getAbsolutePath();
 		return path;
+	}
+
+	@GetMapping("/{id}")
+	public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		String folder = "/Users/songhaibei/springSecurity/spring-security-parent";
+		try (InputStream inputStream = new FileInputStream(new File(folder, id + ".txt"));
+             OutputStream outputStream = response.getOutputStream();) {
+
+			response.setContentType("application/x-download");
+			response.addHeader("Content-Disposition", "attachment;filename=test.txt");
+
+			IOUtils.copy(inputStream, outputStream);
+			outputStream.flush();
+		}
 	}
 
 }
