a212777193e0a5ebe08d4b540f04dba875953f63
baby7305 <songhaibei7305@163.com>
2018-10-16 18:33:54 +0800

    设置拦截异步请求

--- spring-security-demo/src/main/java/com/example/config/WebConfig.java
+++ spring-security-demo/src/main/java/com/example/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import com.example.interceptor.TimeInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private TimeInterceptor timeInterceptor;
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.registerCallableInterceptors();
+        configurer.registerDeferredResultInterceptors();
+        configurer.setDefaultTimeout(5000);//自定义超时时间
+        configurer.setTaskExecutor();//自定义线程池
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
