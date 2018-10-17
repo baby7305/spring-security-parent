30def461f0b4e4c7920cf4ffd7b6cf39fe376d63
baby7305 <songhaibei7305@163.com>
2018-10-17 14:41:47 +0800

    自定义类实现UserDetailsService接口，接收用户名和密码，用算法加密密码，和数据库取出加密字符串比对，通过验证调用api。

---
+++ spring-security-browser/src/main/java/com/example/browser/webconfig/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.browser.webconfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("登录用户名：{}", username);
+        //根据用户名查找用户信息，前端密码0，下面的加密字符串从数据库拿
+        return new User(username, "$2a$10$/eEV4X7hXPzYGzOLXfCizu6h7iRisp7I116wPA3P9uRcHAKJyY4TK", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
+
